4ea211dfef88cf21ad4175181b97eaa22216ca93
surjendu104 <cyber00daddy@gmail.com>
2023-04-14 20:52:19 +0530

    feat : updated  DSU implementation

--- Graph algo/DSU/ans.cpp
+++ Graph algo/DSU/ans.cpp
@@ -7,16 +7,18 @@ using namespace std;
 
 class DisjointSet
 {
-    vector<int> rank, parent;
+    vector<int> rank, parent, size;
 
 public:
     DisjointSet(int n)
     {
         rank.resize(n + 1, 0);
         parent.resize(n + 1, 0);
+        size.resize(n + 1);
         for (int i = 0; i <= n; i++)
         {
             parent[i] = i;
+            size[i] = 1;
         }
     }
 
@@ -44,6 +46,21 @@ public:
         {
             parent[ulp_v] = ulp_u;
             rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
         }
     }
 };
